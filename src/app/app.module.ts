3a69e011d11f37970141e26eff4ecde026a2321a
Janosch Zbick <janosch.zbick@gmail.com>
2018-11-06 17:38:22 +0100

    added career widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { AgmCoreModule } from '@agm/core';
 import { WeatherComponent } from './weather/weather.component';
+import { CareerComponent } from './career/career.component';
+import { CareerResultComponent } from './career/career-result/career-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    WeatherComponent
+    WeatherComponent,
+    CareerComponent,
+    CareerResultComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +25,7 @@ import { WeatherComponent } from './weather/weather.component';
     })
   ],
   providers: [],
+  entryComponents: [CareerResultComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
